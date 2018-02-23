000d970a15b86168e02f30972fbd99d80be8ac7a
Thiago Araújo <thiagosoara@gmail.com>
2018-02-23 11:09:40 -0300

    first commit...

---
+++ app/src/main/java/com/example/thiago/exemplo20181/PrimeiraActivity.java
@@ -0,0 +1,13 @@
+package com.example.thiago.exemplo20181;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PrimeiraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_primeira);
+    }
+}
